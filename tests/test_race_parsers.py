1add62ed647d116e5b3fdd17057c2b19a673d5af
soumatsui <gstaf1989@gmail.com>
2026-09-18 18:58:44 +0900

    Add files via upload

--- tests/test_race_parsers.py
+++ tests/test_race_parsers.py
@@ -15,8 +15,8 @@ class RaceParserTest(unittest.TestCase):
     def test_start_exhibition_uses_image_boat_number_not_row_order(self):
         html = (ROOT / 'tests/fixtures/beforeinfo_start_exhibition.html').read_text()
         starts, _ = scraper._parse_start_exhibition(BeautifulSoup(html, 'html.parser'))
-        self.assertEqual(starts[3], {'course': 2, 'st': 0.08})
-        self.assertEqual(starts[2], {'course': 3, 'st': 0.01})
+        self.assertEqual(starts[3], {'course': 2, 'st': 0.08, 'flying': False})
+        self.assertEqual(starts[2], {'course': 3, 'st': 0.01, 'flying': True})
 
     def test_result_finish_order_and_payouts(self):
         html = (ROOT / 'tests/fixtures/raceresult.html').read_text()
